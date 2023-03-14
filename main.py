d3fd6f3a90dfe1f5ebd7711ebccfa067fff05f5a
Macphail Magwira <macphail.magwira@gmail.com>
2023-03-14 17:31:40 +0200

    Added and Trained my RASA model
    Needed something a bit more intelligent to talk to, so decided to use the rasa framework, trained it to identify scientific questions

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import os
 import warnings
 from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
+import spacy
 
 from openfabric_pysdk.context import OpenfabricExecutionRay
 from openfabric_pysdk.loader import ConfigClass
 from time import time
 
+nlp = spacy.load("en_core_web_sm")
 
-############################################################
-# Callback function called on update config
-############################################################
 def config(configuration: ConfigClass):
     # TODO Add code here
     pass
 
-
-############################################################
-# Callback function called on each execution pass
-############################################################
 def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
     output = []
     for text in request.text:
         # TODO Add code here
+        print(text + '\n')
         response = ''
         output.append(response)
 
     return SimpleText(dict(text=output))
+
+
+def main():
+    config(None)
+    print("\n\nWelcome to ScienceBot! I'm here to help you with any questions you have about science.\n\n")
+    
+    while True:
+     user_input = input("What's your question or topic of interest?\n")
+     request = SimpleText(dict(text=[user_input]))
+     result = execute(request, None)
+     print(result.text)
+     
+     if user_input.lower() == 'exit':
+            break
+
+
+if __name__ == '__main__':
+     main()
+  
